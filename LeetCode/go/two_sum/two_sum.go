3800bded55090a24531e7d75b13b1f11d789e179
Wenbin Liu <liuwenbin.lwb@gmail.com>
2022-01-26 16:23:30 +0800

    update for two sum O(n)

--- LeetCode/go/two_sum/two_sum.go
+++ LeetCode/go/two_sum/two_sum.go
@@ -8,5 +8,28 @@ func twoSum(nums []int, target int) []int {
 			}
 		}
 	}
-	return []int{}
+	return nil
+}
+
+func twoSum2(nums []int, target int) []int {
+	for i, v1 := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			if v1+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+func twoSum3(nums []int, target int) []int {
+	hash := map[int]int{}
+	for i, v1 := range nums {
+		p, ok := hash[target-v1]
+		if ok {
+			return []int{p, i}
+		}
+		hash[v1] = i
+	}
+	return nil
 }
--- LeetCode/go/two_sum/two_sum_test.go
+++ LeetCode/go/two_sum/two_sum_test.go
@@ -15,3 +15,15 @@ func TestTwoSum(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestTwoSun3(t *testing.T) {
+	result := twoSum3([]int{2, 7, 11, 15}, 9)
+	if result[0] != 0 || result[1] != 1 {
+		t.Fail()
+	}
+
+	result = twoSum3([]int{3, 2, 4}, 6)
+	if result[0] != 1 || result[1] != 2 {
+		t.Fail()
+	}
+}
