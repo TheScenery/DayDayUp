761b22eaec288b525c24f618651dd770989f1b9b
Wenbin Liu <liuwenbin.lwb@gmail.com>
2022-02-08 23:13:32 +0800

    5 day

--- LeetCode/go/median_of_two_sorted_arrays/solution.go
+++ LeetCode/go/median_of_two_sorted_arrays/solution.go
@@ -59,3 +59,46 @@ func findMedianSortedArrays1(nums1 []int, nums2 []int) float64 {
 
 	return medianOfSortedArray(nums)
 }
+
+func min(x, y int) int {
+	if x <= y {
+		return x
+	}
+	return y
+}
+
+func getKth(nums1, nums2 []int, k int) int {
+	lengthNums1 := len(nums1)
+	lengthNums2 := len(nums2)
+	if lengthNums1 == 0 {
+		return nums2[k-1]
+	}
+	if lengthNums2 == 0 {
+		return nums1[k-1]
+	}
+	if k == 1 {
+		return min(nums1[0], nums2[0])
+	}
+	half := k / 2
+	i := min(lengthNums1-1, half-1)
+	j := min(lengthNums2-1, half-1)
+	x := nums1[i]
+	y := nums2[j]
+	if x <= y {
+		nums1 = nums1[i+1:]
+		k = k - i - 1
+	} else {
+		nums2 = nums2[j+1:]
+		k = k - j - 1
+	}
+	return getKth(nums1, nums2, k)
+}
+
+func findMedianSortedArrays2(nums1 []int, nums2 []int) float64 {
+	length := len(nums1) + len(nums2)
+	half := length / 2
+	if length%2 == 0 {
+		return float64(getKth(nums1, nums2, half)+getKth(nums1, nums2, half+1)) / 2
+	}
+	return float64(getKth(nums1, nums2, half+1))
+}
--- LeetCode/go/median_of_two_sorted_arrays/solution_test.go
+++ LeetCode/go/median_of_two_sorted_arrays/solution_test.go
@@ -28,8 +28,55 @@ func TestFindMedianSortedArrays1(t *testing.T) {
 
 	nums1 = []int{1, 2}
 	nums2 = []int{3, 4}
-	result = findMedianSortedArrays(nums1, nums2)
+	result = findMedianSortedArrays1(nums1, nums2)
 	if result != 2.5 {
+		t.Fatal()
+	}
+}
+
+func TestFindMedianSortedArrays2(t *testing.T) {
+	nums1 := []int{1, 3}
+	nums2 := []int{2}
+	result := findMedianSortedArrays2(nums1, nums2)
+	if result != float64(2) {
+		t.Fatal()
+	}
+
+	nums1 = []int{1, 2}
+	nums2 = []int{3, 4}
+	result = findMedianSortedArrays2(nums1, nums2)
+	if result != 2.5 {
+		t.Fatal()
+	}
+
+	nums1 = []int{1}
+	nums2 = []int{2, 3, 4, 5, 6}
+	result = findMedianSortedArrays2(nums1, nums2)
+	if result != 3.5 {
+		t.Fatal()
+	}
+}
+
+func TestGetKth(t *testing.T) {
+	nums1 := []int{1, 2}
+	nums2 := []int{3, 4}
+	result := getKth(nums1, nums2, 2)
+	if result != 2 {
+		t.Fatal()
+	}
+	result = getKth(nums1, nums2, 3)
+	if result != 3 {
+		t.Fatal()
+	}
+
+	nums1 = []int{1}
+	nums2 = []int{2, 3, 4, 5, 6}
+	result = getKth(nums1, nums2, 3)
+	if result != 3 {
+		t.Fatal()
+	}
+	result = getKth(nums1, nums2, 4)
+	if result != 4 {
 		t.Fatal()
 	}
 }
