9d452af8a8f7aeae509889b355fbd130de9c724b
Wenbin Liu <liuwenbin.lwb@gmail.com>
2022-03-10 22:17:38 +0800

    format code

--- Others/combination-array.js
+++ Others/combination-array.js
@@ -19,37 +19,37 @@
 */
 
 function joinArray(array) {
-	const length = array.length;
-  	if (length >= 2) {
-    	const array1Length = array[0].length;
-      	const array2Length = array[1].length;
-      	const tempArray = [];
-      	for (let i = 0; i < array1Length; i++) {
-        	for (let j = 0; j< array2Length; j++) {
-            	tempArray.push([array[0][i], array[1][j]].join(', '))
-            }
-        }
-      	const newArray = [];
-      	for (let i = 2; i<length;i++) {
-        	newArray.push(array[i]);
-        }
-      	newArray.unshift(tempArray);
-      	return joinArray(newArray);
-    } else {
-    	return array[0];
+  const length = array.length;
+  if (length >= 2) {
+    const array1Length = array[0].length;
+    const array2Length = array[1].length;
+    const tempArray = [];
+    for (let i = 0; i < array1Length; i++) {
+      for (let j = 0; j < array2Length; j++) {
+        tempArray.push([array[0][i], array[1][j]].join(', '))
+      }
     }
+    const newArray = [];
+    for (let i = 2; i < length; i++) {
+      newArray.push(array[i]);
+    }
+    newArray.unshift(tempArray);
+    return joinArray(newArray);
+  } else {
+    return array[0];
+  }
 }
 
 function main(inputValues) {
-	if (!inputValues || !inputValues.length) {
-        return;
-    }
-  	const joidedArray = joinArray(inputValues);
-  	if (joidedArray.length <= 0) {
-    	return
-    }
-  	joidedArray.forEach(result => console.log('[', result, ']'))
+  if (!inputValues || !inputValues.length) {
+    return;
+  }
+  const joidedArray = joinArray(inputValues);
+  if (joidedArray.length <= 0) {
+    return
+  }
+  joidedArray.forEach(result => console.log('[', result, ']'))
 }
 
 
-main([[1,2], [3, 4], [5, 6]])
+main([[1, 2], [3, 4], [5, 6]])
