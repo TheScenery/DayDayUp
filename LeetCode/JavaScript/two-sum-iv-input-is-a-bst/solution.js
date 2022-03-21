682b4527ec8ce5bc2ced933b3feb6fe9f3efb0c4
Wenbin Liu <liuwenbin.lwb@gmail.com>
2022-03-21 22:46:16 +0800

    42 day

---
+++ LeetCode/JavaScript/two-sum-iv-input-is-a-bst/solution.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+ var findTarget = function(root, k) {
+    const set = new Set();
+    const dfs = (root, k) => {
+        if (!root) {
+            return false;
+        }
+        if (set.has(k - root.val)) {
+            return true;
+        }
+        set.add(root.val);
+        return dfs(root.left, k) || dfs(root.right, k);
+    }
+    return dfs(root, k);
+};
