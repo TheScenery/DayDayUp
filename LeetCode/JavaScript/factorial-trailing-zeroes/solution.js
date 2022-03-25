8c8358a8a8e9cf1f32b987114d593cdd6f18b89f
Wenbin Liu <liuwenbin.lwb@gmail.com>
2022-03-26 00:00:35 +0800

    46day

---
+++ LeetCode/JavaScript/factorial-trailing-zeroes/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function (n) {
+    let ans = 0;
+    for (let i = 5; i <= n; i += 5) {
+        for (let x = i; x % 5 == 0; x /= 5) {
+            ++ans;
+        }
+    }
+    return ans;
+};
